b2e8df19c9613fd2a76d4b915a2002cdde5351c1
Nafisha Nepal <nafishanepal@gmail.com>
2026-06-28 13:27:22 +0545

    updated rook.h

--- rook.h
+++ rook.h
@@ -1,1 +1,20 @@
+#ifndef ROOK_H
+#define ROOK_H
 
+#include "Pieces.h"
+#include "Square.h"
+#include <vector>
+using namespace std;
+
+class Rook : public Pieces
+{
+public:
+    Rook(bool isWhite);
+    vector<Square> getMoves(Square cells[][8], int x, int y);
+
+    sf::Texture blackRook;
+    sf::Texture whiteRook;
+    bool hasMoved;
+};
+
+#endif // ROOK_H
