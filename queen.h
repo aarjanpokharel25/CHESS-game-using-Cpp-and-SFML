6a71f6cf3cc6f0fdd31f817d496395a7b78b4772
Nafisha Nepal <nafishanepal@gmail.com>
2026-06-28 13:16:33 +0545

    Code of queen and rook

--- queen.h
+++ queen.h
@@ -1,1 +1,19 @@
+#ifndef QUEEN_H
+#define QUEEN_H
 
+#include "Pieces.h"
+#include "Square.h"
+#include <vector>
+using namespace std;
+
+class Queen : public Pieces
+{
+public:
+    Queen(bool isWhite);
+    vector<Square> getMoves(Square cells[][8], int x, int y);
+
+    sf::Texture blackQueen;
+    sf::Texture whiteQueen;
+};
+
+#endif // QUEEN_H
